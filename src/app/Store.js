3413babbc7051544fc6543f9cbbab2a56bb87762
mosfiqurrahmanbd <mosfiqurrahmanweb@gmail.com>
2025-07-19 13:23:34 +0600

    updated

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counters/countersSlice";
 import postsReducer from "../features/posts/postsSlice";
+import { apiSlice } from "../features/api/apiSlice";
 
 
 const Store = configureStore({
     reducer:{
         counters: counterReducer, 
         posts: postsReducer,
+        [apiSlice.reducerPath]:apiSlice.reducer,
     },
 });
 
